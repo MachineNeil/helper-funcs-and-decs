95bdf1cc08f6b1c1cbb84ba8532ca0351092619d
Neil Peña Martínez <112895385+MachineNeil@users.noreply.github.com>
2024-01-24 20:57:33 +0100

    Delete general_purpose/log2file directory

--- general_purpose/log2file/constants.py
+++
@@ -1,7 +0,0 @@
-FILENAME_TEXT = './log2file/logs/text_log.log'
-
-FILENAME_JSON = './log2file/logs/json_log.log'
-
-FORMAT_TEXT = '%(name)s: %(asctime)s | %(levelname)s | %(filename)s:%(lineno)s | %(process)d >>> %(message)s'
-
-FORMAT_JSON = '%(name)s %(asctime)s %(levelname)s %(filename)s %(lineno)s %(process)d %(session_id)s %(message)s'
