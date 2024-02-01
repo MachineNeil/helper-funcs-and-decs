983105fcc1b1cafbc550c0529958e7acbf2d014f
Neil Peña Martínez <112895385+MachineNeil@users.noreply.github.com>
2024-02-01 18:25:29 +0100

    Create functions.py

---
+++ general_purpose/functions.py
@@ -0,0 +1,46 @@
+from random import sample
+from string import ascii_lowercase, ascii_uppercase, digits, punctuation
+from collections import Counter, OrderedDict
+from typing import Dict, Any, Iterable, List
+
+
+def merge_dictionaries(*dicts: Dict[str, Any]) -> Dict[str, Any]:
+    result = {}
+    for d in dicts:
+        for key, value in d.items():
+            if key in result and isinstance(result[key], dict) and isinstance(value, dict):
+                result[key] = merge_dictionaries(result[key], value)
+            else:
+                result[key] = value
+    return result
+
+
+def unique_elements(_list: Iterable[Any]) -> list[Any]:
+    return list(OrderedDict.fromkeys(_list))
+
+
+def count_occurrences(_list: Iterable[Any]) -> Dict[Any, int]:
+    return dict(Counter(_list))
+
+
+def chunk_file(input_file: str, chunk_size: int) -> List[str]:
+    with open(input_file, 'r') as file:
+        content = file.read()
+        return [content[i:i + chunk_size] for i in range(0, len(content), chunk_size)]
+
+
+def find_common_elements(list1: List[Any], list2: List[Any]) -> List[Any]:
+    return list(set(list1) & set(list2))
+
+
+def reverse_dictionary(dict: Dict[Any, Any]) -> Dict[Any, Any]:
+    return {v: k for k, v in dict.items()}
+
+
+def is_sorted(_list: List[Any]) -> bool:
+    return all(_list[i] <= _list[i + 1] for i in range(len(_list) - 1))
+
+
+def generate_password(length: int = 16) -> str:
+    characters = ascii_lowercase + ascii_uppercase + digits + punctuation
+    return ''.join(sample(characters, length))
